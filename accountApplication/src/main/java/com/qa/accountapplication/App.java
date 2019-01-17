9bfc2089ad199e540e6e59a0eda990a84f56d278
Joe Gurney <jgurnney@gmail.com>
2019-01-17 12:32:26 +0100

    various improvements and redundantcy removal

--- accountApplication/src/main/java/com/qa/accountapplication/App.java
+++ accountApplication/src/main/java/com/qa/accountapplication/App.java
@@ -1,25 +1,11 @@
 package com.qa.accountapplication;
 
-import com.qa.accountaapplication.services.*;;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	private App() {}
 	
     public static void main( String[] args )
     {
-/*		Account john = new Account("John", "Smith", 1234);
-		MapService map = new MapService();
-		Printer print = new Printer();
-		map.addAccount(john, john.getId());
-		String toPrint = print.toString(map.getAccount(john.getId()));
-		print.console(toPrint);
-		GsonTranslator gson = new GsonTranslator();
-		String result = gson.toJsonMap(map.getMap());
-		System.out.println(result);*/
+
     }
 }
