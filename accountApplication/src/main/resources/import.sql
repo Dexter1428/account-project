6339445b89472e1671c11c59c9bed9335939c475
Joe Gurney <jgurnney@gmail.com>
2019-01-17 12:12:16 +0100

    fixed sql import

--- accountApplication/src/main/resources/import.sql
+++ accountApplication/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO Account (id, fName, sName, accountNo) VALUES (1, 'John', 'Smith', '123');
-INSERT INTO Account (id, fName, sName, accountNo) VALUES (2, 'Betty', 'Turner', '153');
-INSERT INTO Account (id, fName, sNAme, accountNo) VALUES (3, 'Greg', 'Holmes', '183');
+INSERT INTO Account (userId, fName, sName, accountNo) VALUES (1, 'John', 'Smith', '123');
+INSERT INTO Account (userId, fName, sName, accountNo) VALUES (2, 'Betty', 'Turner', '153');
+INSERT INTO Account (userId, fName, sNAme, accountNo) VALUES (3, 'Greg', 'Holmes', '183');
