28026e23c45557714cc2b13796d1b5f5eb791945
Joe Gurney <jgurnney@gmail.com>
2019-01-17 14:27:29 +0100

    fixed endpoints for remove and find

--- accountApplication/src/main/java/com/qa/rest/AccountEndpoint.java
+++ accountApplication/src/main/java/com/qa/rest/AccountEndpoint.java
@@ -5,6 +5,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.qa.business.service.AccountService;
@@ -29,17 +30,17 @@ public class AccountEndpoint {
 		return service.addAccount(account);
 	}
 	
-	@Path("removeAccountByName")
+	@Path("removeAccountByName/{name}")
 	@DELETE
 	@Produces({"application/json"})
-	public String removeAccountByName(String fName) {
+	public String removeAccountByName(@PathParam("name") String fName) {
 		return service.removeAccountByName(fName);
 	}
 	
-	@Path("findAccount")
+	@Path("findAccount/{id}")
 	@GET
 	@Produces({"application/json"})
-	public Account findAccount(int id) {
+	public Account findAccount(@PathParam("id") int id) {
 		return service.findAccount(id);
 	}
 }
